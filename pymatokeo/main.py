5f6ca6526a75b97001d46258699f0e647a44df90
Henryle-hd <henrydionizi@gmail.com>
2025-01-21 19:11:24 +0300

    ascee,csee,ftna

--- pymatokeo/main.py
+++ pymatokeo/main.py
@@ -2,73 +2,139 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 
-def response(year,schoolId,current_year):
+CURRENT_SUPPORT_ONLY=['acsee','csee','ftna']
+CURRENT_YEAR = datetime.now().year
+def response(url):
     try:
-        if year>=2015 and year<=current_year-2:
-            url=f'https://onlinesys.necta.go.tz/results/{year}/csee/results/{schoolId}.htm'
-            response=requests.get(url)
-            response.raise_for_status()
-            return response
-        else:
-            print(f"Invalid year. Please choose a year between 2015 and the {current_year-2}.")
-            exit()
+        response=requests.get(url)
+        response.raise_for_status()
+        return response
     except requests.exceptions.RequestException as e:
         print(f"Error occurred: {e}")
+        exit()
 
-def table_tr(studentId:str):
-    current_year = datetime.now().year
+def determine_y_url_(studentId:str,level:str)->tuple:
     year=int(studentId.split('/')[2])
     schoolId=studentId.split('/')[0].lower()
-    try:
-        soup= BeautifulSoup(response(year,schoolId,current_year).content,'html.parser')
-        table=soup.find_all('table')
-        if year >=2015 and year <=2018:
-            return table[0].find_all('tr')
-        elif year >=2019 and year <= current_year-1:
-            return table[2].find_all('tr')
+
+    if level.lower() in CURRENT_SUPPORT_ONLY:
+        if level.lower() =='csee':
+            if year>=2015 and year<=CURRENT_YEAR-2:
+                url=f'https://onlinesys.necta.go.tz/results/{year}/csee/results/{schoolId}.htm'
+            else:
+                print(f"Invalid year. Please choose a year between 2015 and the {CURRENT_YEAR-2}.")
+                exit()
+        elif level.lower() =='acsee':
+            if year>=2014 and year<=CURRENT_YEAR-1:
+                url=f'https://onlinesys.necta.go.tz/results/{year}/acsee/results/{schoolId}.htm'
+            else:
+                print(f"Invalid year. Please choose a year between 2014 and the {CURRENT_YEAR-1}.")
+                exit()
+        elif level.lower() =='ftna':
+            if year>=2024 and year<=CURRENT_YEAR-1:
+                url=f'https://matokeo.necta.go.tz/results/{year}/ftna/FTNA{year}/{schoolId.upper()}.htm'
+            elif year>=2022 and year<=2023:
+                url=f'https://onlinesys.necta.go.tz/results/{year}/ftna/results/{schoolId.upper()}.htm'
+            else:
+                print(f"We provide only rersult from 2022 and {CURRENT_YEAR-1} for now, for FTNA (Form Two).")
+                exit()
         else:
-            print("Invalid year. Please choose a year between 2015 and the current year.")
+            print("current level not supported, we're working on it")
+    return url,year
+
+# print(determine_y_url_('S1144/0501/2024','ftna'))
+def table_tr(studentId:str,level:str):
+    url,year=determine_y_url_(studentId,level)
+    try:
+        soup= BeautifulSoup(response(url=url).content,'html.parser')
+        table=soup.find_all('table')
+        if level=='csee':
+            if year >=2015 and year <=2018:
+                return table[0].find_all('tr')
+            elif year >=2019 and year <= CURRENT_YEAR-1:
+                return table[2].find_all('tr')
+            else:
+                print(f"We support only years between 2014 and {CURRENT_YEAR-1}.")
+        elif level=='acsee':
+            if year >=2014 and year <=2019:
+                return table[0].find_all('tr')
+            elif year >=2020 and year <= CURRENT_YEAR-1:
+                return table[2].find_all('tr')
+            else:
+                print(f"We support only years between 2014 and {CURRENT_YEAR-1}.")
+        elif level=='ftna':
+            if year >=2022 and year <= CURRENT_YEAR-1:
+                return table[2]
+            # elif year >=2017 and year <= 2021:
+            #     return table[0].find_all('tr')
+            else:
+                print(f"We provide only rersult from 2022 and {CURRENT_YEAR-1} for now, for FTNA (Form Two).")
+        else:
+            print("current level not supported, we're working on it")
+            exit()
     except Exception as e:
         print(f"Error occurred: {e}")
 
+# print(table_tr('P0104/0001/2024','ftna').find_all('tr'))
+# with open('results.html','w') as file:
+#     file.write(str(table_tr('P0104/0001/2024','ftna')))
 
+def get_headers(studentId:str,level:str):
+    """ Get the headers of the table. By accessing the first row of the table."""
 
-def get_headers(studentId:str):
-    list_headers=[]
-    for iteam in table_tr(studentId)[0]:
-        try:
-            header=iteam.get_text().replace("\n",'').replace("\r",'').strip()
-            if header:
-                list_headers.append(header)
-        except:
-            continue
+    if level == 'ftna':
+        header_row = table_tr(studentId,level).find('tr')
+        list_headers=[header.get_text(strip=True) for header in header_row.find_all('p')]
+    else:
+        list_headers=[]
+        for iteam in table_tr(studentId,level)[0]:
+            try:
+                header=iteam.get_text().replace("\n",'').replace("\r",'').strip()
+                if header:
+                    list_headers.append(header)
+            except:
+                continue
     return list_headers
 
-# print(get_headers(studentId))
+# print(get_headers('P0104/0001/2023','csee'))
 
-def get_results(studentId:str):
-    result=[]
-    for element in table_tr(studentId)[1:]:
-        result_row=[]
+def get_data(studentId:str,level:str):
+    """ Get the data of the table. By accessing the second row .... n row of the table."""
+    data=[]
+    for element in table_tr(studentId,level)[1:]:
+        data_row=[]
         for item in element:
             try:
                 row_item=item.get_text().replace("\n",'').replace("\r",'').strip()
                 if row_item:
-                    result_row.append(row_item)
+                    data_row.append(row_item)
             except:
                 continue
-        result.append(result_row)
-    return result
-# print(get_results(studentId))
+        data.append(data_row)
+    return data
 
-def result_to_truple(studentId):
+def get_data_ftna(studentId:str,level:str):
+    '''Geting the data from the ftna table, By accessing the second row .... n row of the table.'''
+    header_row = table_tr(studentId,level).find('tr')
+    headers=[header.get_text(strip=True) for header in header_row.find_all('p')]
+    rows = table_tr(studentId,level).find_all('td')[len(headers):]
+    # Group data into rows based on the number of headers
+    data = []
+    current_row = []
+    for cell in rows:
+        cell_text = cell.get_text(strip=True)
+        current_row.append(cell_text)
+        if len(current_row) == len(headers):
+            record = tuple(current_row)
+            data.append(record)
+            current_row = []
+    return data
+
+# print(get_data_ftna('P0104/0001/2024','ftna'))
+
+def result_to_truple(studentId:str,level:str)->tuple:
     result=[]
-    for row in get_results(studentId):
-        # cno=row[0]
-        # sex=row[1]
-        # aggt=row[2]
-        # div=row[3]
-        # subjects=row[4].split('-')
+    for row in get_data(studentId,level):
         result.append(tuple(row))
     return result
 
@@ -85,29 +151,66 @@ def searchStudentResults(studentId,results):
             lo=mid+1
         else:
             hi=mid-1
-    return ""
+    return (
+        'NOT FOUND',
+        'NOT FOUND',
+        'NOT FOUND',
+        'NOT FOUND',
+        'NOT FOUND',
+    )
 
 
-def dictStudentResults(studentId):
-    heandes=get_headers(studentId)
-    results=result_to_truple(studentId)
+def dictStudentResults(studentId:str,level:str)->dict:
+    '''return single student results in dictionary format
+        {'CNO': 'P0134/0014',
+        'SEX': 'F',
+        'AGGT': '-',
+        'DIV': '0',
+        'DETAILED SUBJECTS': "CIV - 'F'   HIST - 'F'   GEO - 'F'   KISW - 'F'   ENGL - 'F'"}
+    '''
+    headers=get_headers(studentId,level)
+    if level=='ftna':
+        results=get_data_ftna(studentId,level)
+    else:
+        results=result_to_truple(studentId,level)
     student_results=searchStudentResults(studentId,results)
     result_dict=dict(
-        zip(heandes,student_results)
+        zip(headers,student_results)
     )
     return result_dict
 
 
-def ffour_r(studentId):
-    st_r=dictStudentResults(studentId)
-    if st_r =='':
-        print('Not found')
-    else:
-        for key, value in st_r.items():
-            print(f'{key}: {value}')
+class Pymatokeo:
+    def __init__(self):
+       pass
+    def matokeo(self,studentId:str,level:str):
+        self.studentId=studentId
+        self.level=level
+        self.headers=get_headers(studentId,level)
+        self.results=get_data(studentId,level)
+        self.student_results=searchStudentResults(studentId,self.results)
+        self.result_dict=dict(
+            zip(self.headers,self.student_results)
+        )
+        return self.result_dict
+    def __str__(self):
+        return str(self.result_dict)
+    def __repr__(self):
+        return str(self.result_dict)
 
 if __name__ == '__main__':
     while True:
+        level=int(input('level(1. csee/ 2. acsee/ 3. ftna): ').strip())
         studentId=input('student_id(XXXXX/XXXX/XXXX): ').strip()
-        ffour_r(studentId)
+        if level==1:
+            r=Pymatokeo().matokeo(studentId,'csee')
+        elif level==2:
+            r=Pymatokeo().matokeo(studentId,'acsee')
+        elif level==3:
+            r=Pymatokeo().matokeo(studentId,'ftna')
+        else:
+            print("choose a valid level!")
+        for i in r:
+            print(f'{i}: {r[i]}')
+        print('---------------------------------------')
 
--- pymatokeo/t.py
+++ pymatokeo/t.py
@@ -1,5 +1,15 @@
-from main import ffour_r
-
+from main import Pymatokeo
 while True:
+        level=int(input('level(1. csee/ 2. acsee/ 3. ftna): ').strip())
         studentId=input('student_id(XXXXX/XXXX/XXXX): ').strip()
-        ffour_r(studentId)
+        if level==1:
+            r=Pymatokeo().matokeo(studentId,'csee')
+        elif level==2:
+            r=Pymatokeo().matokeo(studentId,'acsee')
+        elif level==3:
+            r=Pymatokeo().matokeo(studentId,'ftna')
+        else:
+            print("choose a valid level!")
+        for i in r:
+            print(f'{i}: {r[i]}')
+        print('---------------------------------------')
